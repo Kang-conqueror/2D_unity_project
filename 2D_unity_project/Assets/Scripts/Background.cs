56477fe76b6f6c5c0cdb09415a383386b66a8f13
Kang-conqueror <139243766+Kang-conqueror@users.noreply.github.com>
2023-07-12 22:34:31 +0900

    자동완성 오류 고침?
    자동완성이 안되는 오류 수정?
    
    잘 됬는지는 모르겠다 지금도 됬다가 안됬다가 반복하고 omnisharp은 계속 error 일으키며 난리를 친다

--- 2D_unity_project/Assets/Scripts/Background.cs
+++ 2D_unity_project/Assets/Scripts/Background.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class backgrounds : MonoBehaviour
+public class Background : MonoBehaviour
 {
     // Start is called before the first frame update
-    // void Start()
-    // {
+    void Start()
+    {
         
-    // }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.position
     }
 }
